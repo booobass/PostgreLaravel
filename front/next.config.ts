797268762789decc84c4d0c06a9e092d82aa9f2d
BoooBass <revive.elephant@gmail.com>
2025-11-20 09:30:14 +0900

    image

--- front/next.config.ts
+++ front/next.config.ts
@@ -3,19 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/storage/images/**',
-      },
-      {
-        protocol: "https",
-        hostname: "postgrelaravel-production.up.railway.app",
-        pathname: "/storage/images/**",
-      }
-    ]
+    unoptimized: true
+    // remotePatterns: [
+    //   {
+    //     protocol: 'http',
+    //     hostname: 'localhost',
+    //     port: '8000',
+    //     pathname: '/storage/images/**',
+    //   },
+    //   {
+    //     protocol: "https",
+    //     hostname: "postgrelaravel-production.up.railway.app",
+    //     pathname: "/storage/images/**",
+    //   }
+    // ]
   }
 };
 
